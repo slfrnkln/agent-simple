703ecbcae69ccdbc0b8d2e01f427a1e960f1b978
Josh <joshscroft@gmail.com>
2019-05-26 17:05:34 +0100

    ini

---
+++ workdir/agent/agent_dataModel.py
@@ -0,0 +1,16 @@
+from typing import List
+import os, sys
+
+from oef.query import Eq, Range, Constraint, Query, AttributeSchema
+from oef.schema import DataModel, Description , Location
+
+
+class TIME_AGENT (DataModel):
+	ATTRIBUTE_TIMEZONE = AttributeSchema("timezone", str, True, "TimeZone")
+	ATTRIBUTE_ID = AttributeSchema("id", str, True, "Id")
+
+	
+	def __init__(self):
+		super().__init__("time_datamodel",[self.ATTRIBUTE_TIMEZONE,
+									  	 			 self.ATTRIBUTE_ID],
+									  	 			 "Agent has the time.")
